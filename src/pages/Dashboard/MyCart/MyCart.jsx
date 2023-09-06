a50ca368e913c3dc1da43b1fa985e5f4d6bbc0ac
EngrArfin <arfin.cse.green.edu.bd.gmail.com>
2023-09-06 21:39:01 +0600

    update delete

--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -2,18 +2,41 @@ import { Helmet } from "react-helmet";
 import useCart from "../../../hooks/useCart";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
   const [cart] = useCart();
   console.log(cart);
+  //how does reduce work
   const total = cart.reduce((sum, item) => item.price + sum, 0);
+  const handleDelete = item =>{
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch('')
+      }
+    })
+  }
+ /*  Swal.fire(
+    'Deleted!',
+    'Your file has been deleted.',
+    'success'
+  ) */
+
   return (
     <section>
        <SectionTitle
         heading={"Wanna Add More"}
         subHeading={"--My Cart--"}
       ></SectionTitle> 
-      <div>
+      <div className="w-full">
         <Helmet>
           <title>Bistro Boss | My Cart</title>
         </Helmet>
@@ -52,7 +75,7 @@ const MyCart = () => {
                   <td>{item.name}</td>
                   <td className="text-end">${item.price}</td>
                   <td>
-                    <button className="btn btn-ghost btn-sm text-white bg-red-600">
+                    <button onClick={() => handleDelete(item)} className="btn btn-ghost btn-sm text-white bg-red-600">
                       <FaTrashAlt></FaTrashAlt>
                     </button>
                   </td>
